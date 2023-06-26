f3bf894acdb0e4eb607961e42ee55f7f829b0901
Nathan Williams <nate88williams@gmail.com>
2023-06-26 15:52:03 -0500

    Added javascript to generate the div table. Size of table is injected next to dimension button. Current input is the default 16x16. No user input functionality was added.

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ const updateDimensionText = () => {
 
 const createGrid = (cellNumber = 16) => {
     let grid = document.querySelector('#grid-area');
-    
     for (let i = 0; i < cellNumber; i++) {
-        let cell = document.createElement('div');
-        cell.classList.add('grid-cell');
-        cell.textContent = 'test'; //Test
-        grid.appendChild(cell);
+        let cellColumn = document.createElement('div');
+        cellColumn.classList.add('grid-cell-column');
+        grid.appendChild(cellColumn);
+        for (let i = 0; i < cellNumber; i++) {
+            let cellRow = document.createElement('div');
+            cellRow.classList.add('grid-cell-row');
+            cellRow.textContent = 'test'; //Test
+            grid.lastChild.appendChild(cellRow);
+        }
     }
 }
 
