834c53c8cb1c1e1d18094ba715e5c3871467e884
Nathan Williams <nate88williams@gmail.com>
2023-06-26 22:56:44 -0500

    Added user input for table. Also added cell shading in the table when the user mouses over each cell. Additional styling and html was added to facilitate recent changes.

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
-let dimension = '16x16'
-document.querySelector('#dimension-text').textContent = dimension;
+/**************************************************
+ * SCRIPT FUNCTIONS
+ **************************************************/
 
-const updateDimensionText = () => {
-    document.querySelector('#dimension-text').textContent = dimension;
+const dimensionButton = document.querySelector('#create-grid-button');
+
+dimensionButton.addEventListener('click', () => {
+    let dimension = 0;
+    do {
+        try {
+            dimension = parseInt(prompt('Choose a dimension from 1-100 for the grid.'));
+        } catch (error) {
+            console.error(error);
+            dimension = -1;
+        }
+    } while (dimension < 0 || dimension > 100);
+
+    updateDimensionText(dimension)
+    createGrid(dimension);
+})
+
+const updateDimensionText = (dimension) => {
+    document.querySelector('#dimension-text').textContent = `${dimension}x${dimension}`;
 }
 
-const createGrid = (cellNumber = 16) => {
+const createGrid = (dimension) => {
     let grid = document.querySelector('#grid-area');
-    for (let i = 0; i < cellNumber; i++) {
+
+    while (grid.hasChildNodes()) {
+        let child = grid.lastChild;
+        grid.removeChild(child);
+    }
+
+    for (let i = 0; i < dimension; i++) {
         let cellColumn = document.createElement('div');
         cellColumn.classList.add('grid-cell-column');
         grid.appendChild(cellColumn);
-        for (let i = 0; i < cellNumber; i++) {
+        for (let i = 0; i < dimension; i++) {
             let cellRow = document.createElement('div');
             cellRow.classList.add('grid-cell-row');
-            cellRow.textContent = 'test'; //Test
+            cellRow.textContent = '';
             grid.lastChild.appendChild(cellRow);
         }
     }
+
+    let gridCell = document.querySelectorAll('.grid-cell-row');
+    for (let i = 0; i < gridCell.length; i++) {
+        gridCell[i].addEventListener('mouseover', (event) => {
+        gridCell[i].classList.add('grid-cell-row-shaded');
+        })
+    }
 }
 
-createGrid();
+
+/**************************************************
+ * SCRIPTS TO RUN ON FIRST RENDER
+ **************************************************/
+updateDimensionText(16);
+createGrid(16);
